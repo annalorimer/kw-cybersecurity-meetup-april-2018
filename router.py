da4a2f6f45ef4324302f180460ca29b6cfbb1a7b
annalorimer <aemhlori@uwaterloo.ca>
2018-04-26 14:53:56 -0400

    bare minimum of flask app done

--- router.py
+++ router.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, world!"
+# Global vars
+AUTHENTICATED = False
+
+def authenticated():
+    return AUTHENTICATED
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        data = request.form
+        if data["username"] == "admin" and data["password"] == "admin":
+            print "user/pass is correct"
+            AUTHENTICATED = True
+            return render_template("settings.html")
+    else:
+        message = "You need to login to access the router settings"
+        return render_template("index.html", message=message)
+
+
+#@app.route('/settings')
+#def settings():
+    
