323349335bba52bff9ad2f959a2c803cdad1d5d3
annalorimer <aemhlori@uwaterloo.ca>
2018-04-27 15:13:45 -0400

    Authentication works

--- router.py
+++ router.py
@@ -2,25 +2,43 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Debug stuff
+
+def debug(msg):
+    print msg
+
 # Global vars
 AUTHENTICATED = False
 
-def authenticated():
+def is_authenticated():
     return AUTHENTICATED
+
+def authenticate():
+    debug("Authenticated!")
+    global AUTHENTICATED 
+    AUTHENTICATED = True
+
+def unauthenticate():
+    global AUTHENTICATED 
+    AUTHENTICATED = False
 
 @app.route('/', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
         data = request.form
+        # Yeah yeah this is hardcoded sue me I didn't have time to hash and salt it then store it in a db
         if data["username"] == "admin" and data["password"] == "admin":
-            print "user/pass is correct"
-            AUTHENTICATED = True
-            return render_template("settings.html")
+            authenticate()
+    
+    if is_authenticated():
+        debug("Rendering settings ...")
+        return render_template("/settings.html")
     else:
+        debug("Must authenticate to proceed")
         message = "You need to login to access the router settings"
         return render_template("index.html", message=message)
 
 
-#@app.route('/settings')
+#@app.route('/settings', methods=["GET", "POST"])
 #def settings():
     
